8dff6f0c67d0eb8b5c48a1acff9dd3ddc5996efc
YutoUrushima <yuto259233@gmail.com>
2023-02-02 18:11:10 +0900

    Finish var

--- main.go
+++ main.go
@@ -2,16 +2,46 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-// HelloWorld
-/*
-multiple
-lines
-*/
-
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println(time.Now())
+	/*
+		var i int = 1
+		fl64 := float64(i)
+		fmt.Println(fl64)
+		fmt.Printf("i = %T\n", i)
+		fmt.Printf("fl64 = %T\n", fl64)
+
+		i2 := int64(fl64)
+		fmt.Printf("i2 = %T\n", i2)
+
+		fl := 10.5
+		i3 := int(fl)
+		fmt.Printf("i3 = %T\n", i3)
+		fmt.Println(i3)
+	*/
+
+	// var s string = "100"
+	// fmt.Printf("s = %T\n", s)
+
+	// i, _ := strconv.Atoi("A")
+	/*
+		if err != nil {
+			fmt.Println(err)
+		}
+	*/
+	// fmt.Println(i)
+	// fmt.Printf("i = %T\n", i)
+
+	// var i2 int = 200
+	// s2 := strconv.Itoa(i2)
+	// fmt.Println(s2)
+	// fmt.Printf("s2 = %T\n", s2)
+
+	var h string = "Hello World"
+	b := []byte(h)
+	fmt.Println(b)
+
+	h2 := string(b)
+	fmt.Println(h2)
 }
